f8cf877476a899e8393e5b618bb93dbd9bc7a842
Peenchayakorn <peen.ctm101@gmail.com>
2022-08-31 10:52:45 +0700

    php

---
+++ post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+<center><h1>Peenchayakorn Webboard</h1></center>
+    <hr>
+    <br>
+<center>
+    <table style="border:2px solid black; width:40%" align="center">
+    หมายเลขกระทู้ <?php echo $_GET["id"] ?> <br> <br> 
+    <tr><td style="background-color:#6cd2fe;" colspan = "2">แสดงความคิดเห็น</tr></td>
+    <tr><td><textarea id="test" name="test" rows="10" cols="70"></textarea> </td></tr>
+    <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+    </table>
+    <br><center><a href="index.html">กลับไปหน้าหลัก</a></center>
+</center>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+<center><h1>Peenchayakorn Webboard</h1></center>
+    <hr>
+    <br>
+<center>
+    เข้าสู่ระบบด้วย <br>
+    Login = <?php echo $_POST["login"]; ?> <br>
+    Password = <?php echo $_POST["pwd"]; ?>
+</center>
+</body>
+</html>
+
